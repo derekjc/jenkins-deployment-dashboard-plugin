bb8b95d35dad0934e6de660572cc801a789f8801
Derek Cordeiro <derekcordeiro@gmail.com>
2017-05-05 03:52:19 +0000

    Added new regions to AwsRegion.java

--- src/main/java/de/codecentric/jenkins/dashboard/impl/environments/ec2/AwsRegion.java
+++ src/main/java/de/codecentric/jenkins/dashboard/impl/environments/ec2/AwsRegion.java
@@ -8,11 +8,17 @@ package de.codecentric.jenkins.dashboard.impl.environments.ec2;
 public enum AwsRegion {
 
     AP_NORTHEAST_1("ap-northeast-1", "Asia Pacific (Tokyo) Region"), //
+    AP_NORTHEAST_2("ap-northeast-2", "Asia Pacific (Seoul) Region"), //
+    AP_SOUTH_1("ap-south-1", "Asia Pacific (Mumbai) Region"), //
     AP_SOUTHEAST_1("ap-southeast-1", "Asia Pacific (Singapore) Region"), //
     AP_SOUTHEAST_2("ap-southeast-2", "Asia Pacific (Sydney) Region"), //
+    CA_CENTRAL_1("ca-central-1", "Canada (Central) Region"), //
+    EU_CENTRAL_1("eu-central-1", "EU (Frankfurt) Region"), //
     EU_WEST_1("eu-west-1", "EU (Ireland) Region"), //
+    EU_WEST_2("eu-west-2", "EU (London) Region"), //
     SA_EAST_1("sa-east-1", "South America (Sao Paulo) Region"), //
     US_EAST_1("us-east-1", "US East (Northern Virginia) Region"), //
+    US_EAST_2("us-east-2", "US East (Ohio) Region"), //
     US_WEST_1("us-west-1", "US West (Northern California) Region"), //
     US_WEST_2("us-west-2", "US West (Oregon) Region");
 
